91bbdbc6e58223ccd6a16f14e8ef1ff7110ff407
Phalguni Mahajan <SEZ6471@neilsoft.in>
2024-10-04 19:13:37 +0530

    create product

--- src/pages/admin/products/CreateProduct.js
+++ src/pages/admin/products/CreateProduct.js
@@ -1,4 +1,48 @@
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
 export function CreateProduct(){
+
+        const navigate=useNavigate()
+
+
+    async function handleSubmit(event){
+
+        event.preventDefault();
+
+        const formData= new FormData(event.target);
+
+        const product= Object.fromEntries(formData.entries());
+
+        if(!product.name || !product.price){
+            alert("field required");
+            return
+        }
+
+
+            try{
+                 const response=await fetch("http://localhost:4000/products",{
+                    method:"POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify(product), // Send data as JSON
+                    });
+
+                const data=await response.json();
+
+                if(response.ok){
+                    navigate("/admin/products")
+                }
+                else if(response.status===400){alert("not found")}
+                else{alert("NOT CREATE PRODUCT")};
+             }
+
+            catch(error){
+                alert("Unable to connect to server")
+
+            }
+
+    }
 
     return(
 
@@ -10,31 +54,42 @@ export function CreateProduct(){
 
                     <h2 className="text-centre mb-5">CREATE PRODUCT</h2>
 
-                    <form>
+                    <form onSubmit={handleSubmit}>
+
                         <div className="row mb-3">
 
                             <label className="col-sm-4 col-form-label">Name</label>
-                            <input className="form-contro" name="name"></input>
+                           
+                           <div className="col-sm-8">
+                            <input className="form-control" name="name"></input>
                             <span className="text-danger"></span>
+                            </div>
 
                          </div>
 
                       
 
-                            <div className="row mb-3">
+                         <div className="row mb-3">
+              <label className="col-sm-4 col-form-label">Price</label>
+              <div className="col-sm-8">
+                <input className="form-control" name="price" type="number"/>
+                <span className="text-danger"></span>
+              </div>
+            </div>
 
-                                <label className="col-sm-4 col-form-label">Price</label>
-                                
-                                <div className="col-sm-8">
-                                    <select className="form-select" name="category">
-                                        <option value='Other'>Other</option>
-                                        <option value='6554'>6554</option>
-                                        <option value='234'>234</option>
-                                        <option value='35'>35</option>
-                                    </select>
-                                </div>
 
-                                <span className="text-danger"></span>
+
+                                <div className="row mb-3">
+
+                                    <div className="offset-sm-4 col-sm-4 d-grid">
+                                        <button type="submit" className="btn btn-primary" >Submit</button>
+                                    </div>
+
+                                    <div className="col-sm-4 d-grid">
+                                        <Link className="btn btn-secondary" to='/admin/produts' role='button'>Cancel</Link>
+                                    </div>
+
+                                  
 
                                 </div>
 
